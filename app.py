a1877f25025cc2936090b2ed17e613b39e7e97f0
Elizabeth Thorne <thorneel@flip1.engr.oregonstate.edu>
2022-06-03 10:30:17 -0700

    add edit_student/id app route

--- app.py
+++ app.py
@@ -88,6 +88,40 @@ def delete_student(student_id):
     # redirect back to Students page
     return redirect("/students")
 
+
+@app.route('/edit_student/<int:student_id>', methods=['POST', 'GET'])
+def edit_student(student_id):
+    if request.method == 'GET':
+        # mySQL query to grab the info of the student with our passed student_id
+        query = """
+        SELECT Students.student_id AS "ID", Students.first_name AS First, Students.last_name AS Last, 
+        Students.school_email AS Email, Students.phone_number AS Phone, 
+        Students.address_line1 AS "Address Line 1", Students.address_line2 AS "Address Line 2", 
+        Students.city AS City, Students.state AS State, Students.postal_code AS "Postal Code", 
+        Majors.title AS Major, 
+        CONCAT(Advisors.first_name, ' ', Advisors.last_name) AS Advisor
+        FROM Students 
+        INNER JOIN Advisors ON Students.advisor_id = Advisors.advisor_id
+        LEFT JOIN Majors ON Students.major_id = Majors.major_id
+        WHERE student_id = %s;
+        """ % (student_id)
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        data = cursor.fetchall()
+
+        # Populate Majors dropdown
+        query2 = 'SELECT major_id, title FROM Majors;'
+        cursor = db.execute_query(db_connection=db_connection, query=query2)
+        majors = cursor.fetchall()
+
+        # Populate Advisors dropdown
+        query3 = "SELECT advisor_id, CONCAT(first_name, ' ', last_name) AS Advisor FROM Advisors;"
+        cursor = db.execute_query(db_connection=db_connection, query=query3)
+        advisors = cursor.fetchall()
+
+        # render students_edit page passing our query data, majors and advisors to the edit_student template
+        return render_template('students_edit.j2', data=data, majors=majors, advisors=advisors, student_id=student_id)
+
+
 @app.route('/majors', methods=['POST', 'GET'])
 def majors():
 
